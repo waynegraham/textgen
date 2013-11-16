0bd7e2039b313b04c065e29a3c0fb27d7a2fa753
Wayne Graham <wayne.graham@virginia.edu>
2013-11-15 23:35:00 -0500

    Download some texts

---
+++ downloader.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+curl -o texts/pg2600.txt http://www.gutenberg.org/cache/epub/2600/pg2600.txt # War and Peace
+curl -o texts/pg16328.txt http://www.gutenberg.org/cache/epub/16328/pg16328.txt # Beowolf
+curl -o texts/pg5200.txt http://www.gutenberg.org/cache/epub/5200/pg5200.txt # Metamorphisis
+curl -o texts/pg1322.txt http://www.gutenberg.org/cache/epub/1322/pg1322.txt # Leaves of Grass
+curl -o texts/pg1232.txt http://www.gutenberg.org/cache/epub/1232/pg1232.txt # The Prince
+
